15c7b1123dd578d71440ab2963a95b08c412b408
Hornos Tamás <tom@raynet.hu>
2018-09-11 15:03:23 +0200

    no message

---
+++ choco_install_default.cmd
@@ -0,0 +1,59 @@
+@ECHO OFF
+
+echo "Web Browser - Google Chrome: https://www.google.com/chrome/"
+choco install -y googlechrome
+
+echo "Editor - Visual Studio Code: https://visualstudio.microsoft.com/"
+choco install -y visualstudiocode
+
+echo "Java - Java SE Runtime Environment"
+choco install -y jre8
+
+echo "System - Windows Sysinternals: https://docs.microsoft.com/en-us/sysinternals/"
+choco install -y sysinternals
+
+echo "System - Syncthing GUI: https://github.com/canton7/SyncTrayzor"
+choco install -y synctrayzor
+
+echo "System - Endpoint visibility: https://osquery.io/"
+choco install -y osquery
+
+echo "Utilities - Windows Directory Statistics: https://windirstat.net/"
+choco install -y windirstat
+
+echo "Utilities - 7-zip Archiver: https://www.7-zip.org/"
+choco install -y 7zip
+
+echo "Utilities - Desktop Skin: https://www.rainmeter.net/"
+choco install -y rainmeter
+
+echo "Security - GPG: https://www.gpg4win.org/index.html"
+choco install -y gpg4win
+
+echo "Security - Crypto: https://cryptomator.org/"
+choco install -y cryptomator
+
+echo "Security - Password: https://keepass.info/"
+choco install -y keepass
+
+echo "Security - SSH: https://winscp.net/eng/index.php"
+choco install -y winscp
+
+echo "Security - SSH: https://www.putty.org/"
+choco install -y putty
+
+echo "Applications"
+choco install -y f.lux
+choco install -y multicommander
+choco install -y freefilesync
+choco install -y greenshot
+choco install -y irfanview
+choco install -y pidgin
+choco install -y skype
+choco install -y vlc
+choco install -y adobereader
+choco install -y edrawings-viewer
+choco install -y nitroshare
+choco install -y systemexplorer
+choco install -y autohotkey
+choco install -y iiscrypto iiscrypto-cli
---
+++ choco_install_optional.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+echo "Optional"
+choco install -y viber --ignore-checksums
+choco install -y google-drive-file-stream --ignore-checksums
+choco install -y gsuite-sync-outlook --ignore-checksums
+choco install -y evernote
+choco install -y spotify --ignore-checksums
+choco install -y deluge
