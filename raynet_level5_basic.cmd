5e45e7ca17a2305043de6dba40a4371dc5a3859e
Tamás Hornos <tom@raynet.hu>
2019-04-15 15:40:15 +0200

    no message

---
+++ raynet_level5_basic.cmd
@@ -0,0 +1,81 @@
+@ECHO OFF
+
+echo "Web Browser - Google Chrome: https://www.google.com/chrome/"
+choco install -y googlechrome
+
+echo "Editor - Visual Studio Code: https://visualstudio.microsoft.com/"
+choco install -y visualstudiocode
+
+REM echo "Java - Java SE Runtime Environment"
+REM choco install -y jre8
+
+REM echo "System - Windows Sysinternals: https://docs.microsoft.com/en-us/sysinternals/"
+REM choco install -y sysinternals
+
+REM echo "System - Syncthing GUI: https://github.com/canton7/SyncTrayzor"
+REM choco install -y synctrayzor
+
+echo "System - Endpoint visibility: https://osquery.io/"
+choco install -y osquery
+
+echo "Utilities - Windows Directory Statistics: https://windirstat.net/"
+choco install -y windirstat
+
+echo "Utilities - 7-zip Archiver: https://www.7-zip.org/"
+choco install -y 7zip
+
+echo "Utilities - Desktop Skin: https://www.rainmeter.net/"
+choco install -y rainmeter
+
+echo "Security - GPG: https://www.gpg4win.org/index.html"
+choco install -y gpg4win
+
+REM echo "Security - Crypto: https://cryptomator.org/"
+REM choco install -y cryptomator
+
+REM echo "Security - Crypto: https://www.veracrypt.fr"
+REM choco install -y veracrypt
+
+REM echo "Security - Password: https://keepass.info/"
+REM choco install -y keepass
+
+REM echo "Security - SSH: https://winscp.net/eng/index.php"
+REM choco install -y winscp
+
+REM echo "Security - SSH: https://www.putty.org/"
+REM choco install -y putty
+
+REM echo "https://chocolatey.org/packages/f.lux"
+REM choco install -y f.lux
+
+REM echo "https://chocolatey.org/packages/cports"
+REM choco install -y cports
+
+REM choco install -y multicommander
+REM choco install -y freefilesync
+
+echo "Total commander file manager"
+choco install -y totalcommander
+
+echo "https://chocolatey.org/packages/greenshot"
+choco install -y greenshot
+
+REM choco install -y irfanview
+REM choco install -y pidgin
+REM choco install -y skype
+
+echo "https://chocolatey.org/packages/vlc"
+choco install -y vlc
+
+echo "https://chocolatey.org/packages/adobereader"
+choco install -y adobereader
+
+REM echo "https://chocolatey.org/packages/edrawings-viewer"
+REM choco install -y edrawings-viewer
+
+REM choco install -y nitroshare
+REM echo "https://chocolatey.org/packages/systemexplorer"
+REM choco install -y systemexplorer
+
+REM choco install -y autohotkey
+REM choco install -y iiscrypto iiscrypto-cli
